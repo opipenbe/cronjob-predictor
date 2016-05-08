58b99544269d326a0ad44e3e49074238bce8d045
Olari Pipenberg <olari.pipenberg@gmail.com>
2016-05-08 12:30:09 +0300

    python pep 8

--- cronjob_predictor.py
+++ cronjob_predictor.py
@@ -11,67 +11,64 @@ from datetime import datetime
 
 #date -d"2016-05-07 05:00:00" +%d
 
-def getCronTable(): #read crontab output into variable
+def get_cron_table(): #read crontab output into variable
     try:
-        cronTable = subprocess.check_output('crontab -l', shell=True)
+        cron_table = subprocess.check_output('crontab -l', shell=True)
     except: #no cron entries
         quit()
-    return cronTable
+    return cron_table
 
-def findCronJobs(cronVar): #append cronjob lines into list
-    cronTable = cronVar
+def find_cron_jobs(cron_table): #append cronjob lines into list
     lines = []
-    for line in cronTable.splitlines():
+    for line in cron_table.splitlines():
         line = line.lstrip() #remove leading whitespaces
-        if validateCronEntry(line) == True:
+        if validate_cron_syntax(line) == True:
             lines.append(line)
     return lines
 
-def validateCronEntry(cronLine): #validates correct crontab lines
-    if not cronLine.strip(): #check for empty lines
+def validate_cron_syntax(cron_line): #validates correct crontab lines
+    if not cron_line.strip(): #check for empty lines
         return False
-    if cronLine.startswith("#"): #check if it is not comment
+    if cron_line.startswith("#"): #check if it is not comment
         return False
-    if cronLine.startswith("@"): #check if it is special string
+    if cron_line.startswith("@"): #check if it is special string
         print("Crontab entry contains special strings. Sorry they are not supported yet :(") #TODO?
         quit()
     return True
 
-def timeLeft(arg):
-    diff = arg-datetime.now()
-    #Todo formating
-    return diff.seconds
-
-def getTime(cronFormat):
+def get_time(cron_format):
     base = datetime.now() #get current system time
     #print "Current time:",base
-    iter = croniter(cronFormat, base)
+    iter = croniter(cron_format, base)
     #TODO error handling, time global var?
     aeg = iter.get_next(datetime)
     #Todo next
     return aeg
 
-def syntaxToTime(cronJobLines): #create new list and replace cron syntax with times. Siia TODO
+def syntax_to_time(cronjob_line_list): #create new list and replace cron syntax with times. Siia TODO
     #TODO better solution, better regex
-    orderedList = []
-    for line in cronJobLines: #convert cron syntax to time
+    converted_list = []
+    for line in cronjob_line_list: #convert cron syntax to time
         muutuja = re.match('^((?:[^ ]* ){4}[^ ]*)',line).group(0)
-        muutuja2 = str(getTime(muutuja)) #hack
+        muutuja2 = str(get_time(muutuja)) #hack
         line = line.replace(muutuja, muutuja2)
-        orderedList.append(line)
-    return orderedList
+        converted_list.append(line)
+    return converted_list
 
-def sortByTime(list):
+def sort_by_time(list):
     return sorted(list)
 
-def firstCronJob(list):
-    return (list[:1] or "") #"" means in case crontab is empty
-
+def first_cron_job(list):
+    try:
+        job = list[0]
+    except IndexError:
+        job = ""
+    return job
 
 if __name__ == "__main__":
-    cronTable = getCronTable()
-    cronJobs = findCronJobs(cronTable)
-    convertedJobs = syntaxToTime(cronJobs)
-    cronJobs = sortByTime(convertedJobs)
-    print firstCronJob(cronJobs)
+    cron_table = get_cron_table()
+    cron_jobs = find_cron_jobs(cron_table)
+    converted_jobs = syntax_to_time(cron_jobs)
+    cron_jobs = sort_by_time(converted_jobs)
+    print first_cron_job(cron_jobs)
 
